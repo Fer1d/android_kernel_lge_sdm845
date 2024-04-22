f1e432f40c1be1d43dc44ad04ded31c3af744dea
Yumi Yukimura <me.cafebabe@gmail.com>
2024-04-22 10:38:08 +0700

    fs: proc: Add PROC_CMDLINE_APPEND_ANDROID_FORCE_NORMAL_BOOT
    For Android 9 launch A/B devices migrating to Android 10 style
    system-as-root, `androidboot.force_normal_boot=1` must be passed in
    cmdline when booting into normal or charger mode. However, it is not
    always possible for one to modify the bootloader to adhere to these
    changes. As a workaround, one can use the presence of the
    `skip_initramfs` flag in cmdline to to decide whether to append the new
    flag to cmdline on the kernel side.
    
    Co-authored-by: jabashque <jabashque@gmail.com>
    Change-Id: Ia00ea2c54e2a7d2275e552837039033adb98d0ff
    [judyln: this tree's fs/proc/cmdline.c has no CONFIG_MACH_LGE block, so only
    the append logic was ported; a bounds check keeps the append inside
    COMMAND_LINE_SIZE.]
    (cherry picked from commit 75081a43f733de9c2140517d2d35bf00e8dc9768)

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -3,34 +3,58 @@
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 
-#if defined(CONFIG_INITRAMFS_IGNORE_SKIP_FLAG)
+#if defined(CONFIG_INITRAMFS_IGNORE_SKIP_FLAG) || \
+	defined(CONFIG_PROC_CMDLINE_APPEND_ANDROID_FORCE_NORMAL_BOOT)
 #include <asm/setup.h>
 #include <linux/slab.h>
 
 #define INITRAMFS_STR_FIND "skip_initramf"
+#endif // CONFIG_INITRAMFS_IGNORE_SKIP_FLAG || CONFIG_PROC_CMDLINE_APPEND_ANDROID_FORCE_NORMAL_BOOT
+
+#ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
 #define INITRAMFS_STR_REPLACE "want_initramf"
 #define INITRAMFS_STR_LEN (sizeof(INITRAMFS_STR_FIND) - 1)
 #endif // CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
 
-#if defined(CONFIG_INITRAMFS_IGNORE_SKIP_FLAG)
+#ifdef CONFIG_PROC_CMDLINE_APPEND_ANDROID_FORCE_NORMAL_BOOT
+#define ANDROID_FORCE_NORMAL_BOOT_STR "androidboot.force_normal_boot=1"
+#endif // CONFIG_PROC_CMDLINE_APPEND_ANDROID_FORCE_NORMAL_BOOT
+
+#if defined(CONFIG_INITRAMFS_IGNORE_SKIP_FLAG) || \
+	defined(CONFIG_PROC_CMDLINE_APPEND_ANDROID_FORCE_NORMAL_BOOT)
 static char proc_command_line[COMMAND_LINE_SIZE];
 
 static void proc_command_line_init(void)
 {
 	char *offset_addr;
+	char *proc_command_line_tail;
 
 	strcpy(proc_command_line, saved_command_line);
 
+#ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
 	offset_addr = strstr(proc_command_line, INITRAMFS_STR_FIND);
-	if (!offset_addr)
-		return;
-	memcpy(offset_addr, INITRAMFS_STR_REPLACE, INITRAMFS_STR_LEN);
-}
+	if (offset_addr)
+		memcpy(offset_addr, INITRAMFS_STR_REPLACE, INITRAMFS_STR_LEN);
 #endif // CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
+
+#ifdef CONFIG_PROC_CMDLINE_APPEND_ANDROID_FORCE_NORMAL_BOOT
+	if (strstr(saved_command_line, INITRAMFS_STR_FIND) &&
+	    strlen(proc_command_line) + sizeof(ANDROID_FORCE_NORMAL_BOOT_STR) <
+		    COMMAND_LINE_SIZE) {
+		// point proc_command_line_tail to the null terminator of the cmdline
+		proc_command_line_tail = proc_command_line + strlen(proc_command_line);
+		memcpy(proc_command_line_tail, " ", 1);
+		memcpy(proc_command_line_tail + 1, ANDROID_FORCE_NORMAL_BOOT_STR,
+		       sizeof(ANDROID_FORCE_NORMAL_BOOT_STR));
+	}
+#endif // CONFIG_PROC_CMDLINE_APPEND_ANDROID_FORCE_NORMAL_BOOT
+}
+#endif // CONFIG_INITRAMFS_IGNORE_SKIP_FLAG || CONFIG_PROC_CMDLINE_APPEND_ANDROID_FORCE_NORMAL_BOOT
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-#if defined(CONFIG_INITRAMFS_IGNORE_SKIP_FLAG)
+#if defined(CONFIG_INITRAMFS_IGNORE_SKIP_FLAG) || \
+	defined(CONFIG_PROC_CMDLINE_APPEND_ANDROID_FORCE_NORMAL_BOOT)
 	seq_printf(m, "%s\n", proc_command_line);
 #else
 	seq_printf(m, "%s\n", saved_command_line);
@@ -52,7 +76,8 @@ static const struct file_operations cmdline_proc_fops = {
 
 static int __init proc_cmdline_init(void)
 {
-#if defined(CONFIG_INITRAMFS_IGNORE_SKIP_FLAG)
+#if defined(CONFIG_INITRAMFS_IGNORE_SKIP_FLAG) || \
+	defined(CONFIG_PROC_CMDLINE_APPEND_ANDROID_FORCE_NORMAL_BOOT)
 	proc_command_line_init();
 #endif
 
