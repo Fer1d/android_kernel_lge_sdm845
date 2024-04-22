fee3e446ccbaa9258cc66498760c907e33df6cc2
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-04-22 10:33:32 +0700

    init: Add CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
    * Ignoring an ignore flag, yikes
    * Also replace skip_initramf with want_initramf (omitting last letter for Magisk since it binary patches that out of kernel, I'm not even sure why we're supporting that mess)
    
    Co-authored-by: Erfan Abdi <erfangplus@gmail.com>
    Change-Id: Ifdf726f128bc66bf860bbb71024f94f56879710f
    [judyln: this tree's fs/proc/cmdline.c has no CONFIG_MACH_LGE block
    (no updated_command_line/proc_cmdline_set), so only the flag handling was
    ported.]
    (cherry picked from commit 77519baf7f33e8962c999cefbbc750f7d0481323)

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -3,9 +3,38 @@
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 
+#if defined(CONFIG_INITRAMFS_IGNORE_SKIP_FLAG)
+#include <asm/setup.h>
+#include <linux/slab.h>
+
+#define INITRAMFS_STR_FIND "skip_initramf"
+#define INITRAMFS_STR_REPLACE "want_initramf"
+#define INITRAMFS_STR_LEN (sizeof(INITRAMFS_STR_FIND) - 1)
+#endif // CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
+
+#if defined(CONFIG_INITRAMFS_IGNORE_SKIP_FLAG)
+static char proc_command_line[COMMAND_LINE_SIZE];
+
+static void proc_command_line_init(void)
+{
+	char *offset_addr;
+
+	strcpy(proc_command_line, saved_command_line);
+
+	offset_addr = strstr(proc_command_line, INITRAMFS_STR_FIND);
+	if (!offset_addr)
+		return;
+	memcpy(offset_addr, INITRAMFS_STR_REPLACE, INITRAMFS_STR_LEN);
+}
+#endif // CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
+
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
+#if defined(CONFIG_INITRAMFS_IGNORE_SKIP_FLAG)
+	seq_printf(m, "%s\n", proc_command_line);
+#else
 	seq_printf(m, "%s\n", saved_command_line);
+#endif
 	return 0;
 }
 
@@ -23,6 +52,10 @@ static const struct file_operations cmdline_proc_fops = {
 
 static int __init proc_cmdline_init(void)
 {
+#if defined(CONFIG_INITRAMFS_IGNORE_SKIP_FLAG)
+	proc_command_line_init();
+#endif
+
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
 }
