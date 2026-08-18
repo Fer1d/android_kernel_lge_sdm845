329c714176fd16d05b7cc64eb11994147caed944
Fer1d <88480801+Fer1d@users.noreply.github.com>
2026-08-18 23:14:17 +0800

    net: mptcp: adapt select_initial_window to KudProject net-arg signature
    The LGE MPTCP tcp_sock_ops expects a sock-based select_initial_window,
    but the KudProject 4.9.337 tcp_select_initial_window takes struct net *.
    Add an adapter wrapper for tcp_specific and pass sock_net() at the MPTCP
    call site.

--- net/mptcp/mptcp_output.c
+++ net/mptcp/mptcp_output.c
@@ -1633,8 +1633,8 @@ void mptcp_select_initial_window(int __space, __u32 mss, __u32 *rcv_wnd,
 	*window_clamp = mpcb->orig_window_clamp;
 	__space = tcp_win_from_space(mpcb->orig_sk_rcvbuf);
 
-	tcp_select_initial_window(__space, mss, rcv_wnd, window_clamp,
-				  wscale_ok, rcv_wscale, init_rcv_wnd, sk);
+	tcp_select_initial_window(sock_net(sk), __space, mss, rcv_wnd,
+				  window_clamp, wscale_ok, rcv_wscale, init_rcv_wnd);
 }
 
 static inline u64 mptcp_calc_rate(const struct sock *meta_sk, unsigned int mss,
